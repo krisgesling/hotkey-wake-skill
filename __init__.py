72f6311b19c34fcd5449fc4a58cc846aa3974357
Kris Gesling <kris.gesling@mycroft.ai>
2021-08-03 11:50:33 +0930

    alternative implementation with MIT licensed library
    keyboard package is required to run as root

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 import time
 
-from pynput import keyboard
+import keyboard
 
 from mycroft import MycroftSkill
 from mycroft.messagebus.message import Message
@@ -16,56 +16,29 @@ class HotKeyWake(MycroftSkill):
         self.on_settings_changed()
 
     def on_settings_changed(self):
-        if self.listener:
-            self.listener.stop()
+        self.stop_keyboard_listener()
         key_combo = self.settings.get('key_combo')
         # get methods default arg may not work if set to empty string.
         if not key_combo:
             self.log.info(
                 'No key combination provided. Please set this in your Skill '
-                'settings. Falling back to default "<ctrl>+<alt>+r".')
-            key_combo = '<ctrl>+<alt>+r'
-            return False
-        self.listener = self.create_listener(key_combo)
+                'settings. Falling back to default "ctrl+alt+r".')
+            key_combo = 'ctrl+alt+r'
+        listener = keyboard.add_hotkey(key_combo, self.handle_hotkey_press)
+
+    def handle_hotkey_press(self):
+        """Actions to perform when the hotkey combo is detected."""
+        self.log.debug("Wake hotkey detected.")
+        self.bus.emit(Message('mycroft.mic.listen'))
+
+    def stop_keyboard_listener(self):
+        """Stop the keyboard listener. Remove all hotkeys"""
         if self.listener:
-            self.listener.start()
-
-    def create_listener(self, key_combo):
-        # Single key press example
-        if '+' in key_combo:
-            listener = self.create_multi_key_listener(key_combo)
-        else:
-            listener = self.create_single_key_listener(key_combo)
-        return listener
-
-    def create_single_key_listener(self, key_combo):
-        def on_press(key):
-            if key == keyboard.KeyCode.from_char(key_combo):
-                self.bus.emit(Message('mycroft.mic.listen'))
-
-        listener = keyboard.Listener(on_press=on_press)
-        return listener
-
-    def create_multi_key_listener(self, key_combo):
-        # Using global hotkeys
-        def on_activate():
-            self.bus.emit(Message('mycroft.mic.listen'))
-
-        def for_canonical(f):
-            return lambda k: f(listener.canonical(k))
-
-        hotkey = keyboard.HotKey(
-            keyboard.HotKey.parse(key_combo),
-            on_activate)
-
-        listener = keyboard.Listener(on_press=for_canonical(hotkey.press),
-                                     on_release=for_canonical(hotkey.release))
-        return listener
+            keyboard.remove_hotkey(self.listener)
+            self.listener = None
 
     def shutdown(self):
-        if self.listener:
-            self.listener.stop()
-
+        self.stop_keyboard_listener()
 
 def create_skill():
     return HotKeyWake()
